398b202fc2a6c7e28a750cf354d0ca618121e14b
Saketh Pallempati <90235687+SAKETH-PALLEMPATI@users.noreply.github.com>
2023-07-04 15:50:51 +0530

    v1

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css'
 export default function Card({ id, RegNo, Name }) {
     return (
         <div className="ag-courses_item">
-            <Link className="ag-courses-item_link" key={id} to={`/marks/${id}`}>
+            <Link className="ag-courses-item_link" style={{textDecoration : "none"}} key={id} to={`/marks/${id}`}>
                 <div className="ag-courses-item_bg"></div>
                 <div className="ag-courses-item_title">{Name}</div>
                 <div className="ag-courses-item_date-box">
